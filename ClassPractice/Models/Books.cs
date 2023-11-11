31ca97f7c07580fdae6fbadf938e7814725131b2
Nurlan <nurlanea@code.edu.az>
2023-11-11 23:24:25 +0400

    first commit

---
+++ ClassPractice/Models/Books.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice.Models
+{
+    internal class Books
+    {
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value.Length < 5)
+                {
+                    Console.WriteLine("5den kicik ola bilmez");
+                    return;
+                }
+                _title = value;
+            }
+        }
+        public readonly int Pages=200;
+     
+
+        protected string Author;
+
+        public string ShowAuthor ()
+        {
+            Author = "Name";
+            return Author;
+        }
+}
+}
+
---
+++ ClassPractice/Program.cs
@@ -0,0 +1,7 @@
+﻿using ClassPractice.Models;
+
+Books book1 = new ();
+book1.Title = "ABCDEF";
+Console.WriteLine(book1.Title);
+Console.WriteLine(book1.Pages);
+Console.WriteLine(book1.ShowAuthor());
